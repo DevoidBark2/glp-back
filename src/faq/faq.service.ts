92c79f58472653c870959b3252aa59720011e843
DevoidBark2 <petros_9002@mail.ru>
2025-01-08 14:58:44 +0300

    fix small faq

--- src/faq/faq.service.ts
+++ src/faq/faq.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable } from '@nestjs/common'
 import { CreateFaqDto } from './dto/create-faq.dto'
 import { UpdateFaqDto } from './dto/update-faq.dto'
 import { FaqEntity } from './entity/faq.entity'
@@ -10,7 +10,7 @@ export class FaqService {
 	constructor(
 		@InjectRepository(FaqEntity)
 		private readonly faqEntityRepository: Repository<FaqEntity>
-	) {}
+	) { }
 	async create(createFaqDto: CreateFaqDto) {
 		const existingFaq = await this.faqEntityRepository.findOne({
 			where: { question: createFaqDto.question }
@@ -27,7 +27,21 @@ export class FaqService {
 	}
 
 	async findAll() {
-		return await this.faqEntityRepository.find()
+		return await this.faqEntityRepository.find({
+			order: {
+				id: "ASC"
+			}
+		})
+	}
+
+	async getById(id: number) {
+		const faq = await this.faqEntityRepository.findOneBy({ id })
+
+		if (!faq) {
+			throw new BadRequestException("Запись не найдена!")
+		}
+
+		return faq
 	}
 
 	async update(updateFaqDto: UpdateFaqDto) {
